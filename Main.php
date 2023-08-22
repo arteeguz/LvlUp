9951d1df4f7f04bb3a33667d707e5b865cacfb90
WeiJian123-tech <55195954+WeiJian123-tech@users.noreply.github.com>
2023-08-22 09:34:15 -0400

    Add PHP file for Beginning Page
    Added a PHP file to start the front end of our Hackathon project. Basic skeleton built within the PHP file. Mostly HTML so far.

---
+++ Main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="A website that recommends music to users">
+        <meta name="keywords" content="music, recommended, recommendations, hackathon, CTP Program">
+        <meta name="author" content="The Uncluded, BidOOF, Guz, FirestormA">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Music Recommender</title>
+        <!--link tags for css-->
+    </head>
+
+    <body>
+        <div>
+            <h1>Hello World</h1>
+        </div>
+
+        <!--script tags for JavaScript at the end-->
+    </body>
+</html>
